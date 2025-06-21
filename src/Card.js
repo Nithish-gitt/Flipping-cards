7145505c85f58fb3b5b9cf9d9081d3153d7e8e6f
Nithish <nithish.vemula@unisys.com>
2025-06-21 17:47:22 +0530

    CSS updates

--- src/Card.js
+++ src/Card.js
@@ -141,7 +141,7 @@ const Card = () => {
     const mySocketId = socket.id;
     const myIndex = players.findIndex(p => p.id === mySocketId);
     setMyColor(myIndex === 0 ? "blue" : "red");
-    document.body.classList.add(`${currentPlayer}-turn`);
+    // document.body.classList.add(`${currentPlayer}-turn`);
     
   };
 
@@ -151,6 +151,7 @@ const Card = () => {
   };
 
   return (
+    <body className={`${currentPlayer}-turn`}>
     <div className={`flip-flop-game ${currentPlayer}-turn`}>
       {showPopup && <RoomPopup onRoomReady={handleRoomReady} />}
       <header>
@@ -188,6 +189,7 @@ const Card = () => {
         </div>
       )}
     </div>
+    </body>
   );
 };
 
